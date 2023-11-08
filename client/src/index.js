b7f454a87a69d90207fa87ffe6b91e7e97d1bd03
Prayag Tandon <tandonprayag99@gmail.com>
2023-11-08 18:32:13 +0530

    Setting up the mui

--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { configureStore } from '@reduxjs/toolkit';
+import globalreducer from "state";
+import { Provider } from 'react-redux';
+
+const store = configureStore({
+  reducer: {
+    global: globalreducer,
+  },
+
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
