8a276cce63780c9ee677664d6dd94d9206a1a694
Prayag Tandon <tandonprayag99@gmail.com>
2023-11-08 20:53:12 +0530

    Setting the dark and light mode

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import FlexBetween from 'components/FlexBetween';
 import { useDispatch } from 'react-redux';
 import { setMode } from 'state';
 import profileImage from "assets/Profile.jpg";
-import { AppBar, Toolbar, useTheme } from '@mui/material';
+import { AppBar, IconButton, InputBase, Toolbar, useTheme } from '@mui/material';
 
 const Navbar = () => {
     const dispatch = useDispatch();
@@ -16,8 +16,40 @@ const Navbar = () => {
             boxShadow: "none",
         }}
     >
-        <Toolbar sx={{ justifyContent: "space-between" }}></Toolbar>
+        <Toolbar sx={{ justifyContent: "space-between" }}>
+            {/* LEFT SIDE */}
+            <FlexBetween>
+                <IconButton onClick={() => console.log('open/close sidebar')}>
+                    <MenuIcon />
+                </IconButton>
+                <FlexBetween
+                    backgroundColor={theme.palette.background.alt}
+                    borderRadius="9px"
+                    gap="3rem"
+                    p="0.1rem 1.5rem"
+                >
+                    <InputBase placeholder="Search..." />
+                    <IconButton>
+                        <Search />
+                    </IconButton>
+                </FlexBetween>
+            </FlexBetween>
+
+            {/* RIGHT SIDE */}
+            <FlexBetween gap="1.5rem">
+                <IconButton onClick={() => dispatch(setMode())}>
+                    {theme.palette.mode === 'dark' ? (
+                        <DarkModeOutlined sx={{ fontSize: "25px" }} />
+                    ) : (
+                        <LightModeOutlined sx={{ fontSize: "25px" }} />
+                    )}
+                </IconButton>
+                <IconButton>
+                    <SettingsOutlined sx={{ fontSize: "25px" }} />
+                </IconButton>
+            </FlexBetween>
+        </Toolbar>
     </AppBar>
 }
 
-export default Navbar
+export default Navbar;
